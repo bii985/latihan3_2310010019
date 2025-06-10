de0ac8680d2b038249e9d6a8e153f72d06854782
bii985 <avatar.dika27@gmail.com>
2025-06-10 14:14:43 +0800

    membuat accessor dan constructor

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -11,5 +11,29 @@ public class Rumus {
     int lebar;
     int hasil;
     
+    public void HitungLuasPersegi(){
+        hasil = sisi * sisi;
+    
+    }
+    public void HitungLuasPersegiPanjang(){
+        hasil = panjang * lebar;
+    
+    }
+    
+    public void setHasil(){
+    this.hasil = hasil;
+    }
+    
+    public int getHasil(){
+    return hasil;
+    }
+    
+    public persegi(){
+    this.sisi = 0;
+    this.panjang = 0;
+    this.lebar = 0;
+    
+    
+    }
     
 }
